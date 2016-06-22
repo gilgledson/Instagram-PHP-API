9101ffa50344d29722adf5af872b041ee2399ca9
Gilgledson souza <gilgledson96@gmail.com>
2016-06-22 11:44:26 -0300

    function for get image id

--- src/Instagram.php
+++ src/Instagram.php
@@ -149,7 +149,24 @@ class Instagram
 
         return $this->_makeCall('users/search', false, $params);
     }
-
+    
+    /**
+     * convert url image in 
+     * 
+     * @param string url 
+     * 
+     * @return id in image
+     */
+     public function getImageId($url)
+     {
+          $json_file = file_get_contents('http://api.instagram.com/oembed?url='.$url);
+          $json_str = json_decode($json_file, true);
+          
+          return $json_str['media_id'];
+     }
+    
+     
+    
     /**
      * Get user info.
      *
